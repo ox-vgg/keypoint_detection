c2e64a430ddd82cd1be8546465051afe2dceebd0
albanie <albanie@robots.ox.ac.uk>
2017-08-05 09:42:32 +0100

    added setup

---
+++ setup_keypoint_detection.m
@@ -0,0 +1,6 @@
+function setup_keypoint_detection()
+%SETUP_KEYPOINT_DETECTION Sets up keypoint_detection by adding its folders 
+% to the Matlab path
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root, [root '/model-train'], [root '/dagnetworks']) ;
